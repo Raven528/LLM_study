ee076bda975cf1bff21a921c37dda57c7a700e2d
Raven528 <2253277187@qq.com>
2025-01-10 16:49:31 +0800

    remove qwen.py

--- my-llms/agents/utils.py
+++ my-llms/agents/utils.py
@@ -3,14 +3,14 @@ import re
 import json
 from openai import OpenAI
 
-client = OpenAI(
+client_qwen = OpenAI(
     # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
     api_key=os.getenv('DASHSCOPE_API_KEY'),
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 def llm_call(prompt, system_prompt = 'You are a helpful assistant.'):
-    response = client.chat.completions.create(
-        model="qwen-plus",   # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+    response = client_qwen.chat.completions.create(
+        model="qwen-plus-1220",   # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
         messages=[
             {'role': 'system', 'content': system_prompt},
             {'role': 'user', 'content': prompt}],
