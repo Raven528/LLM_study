1df1deeee8fd978ef3f29c4bc069f8de817af520
Raven528 <2253277187@qq.com>
2025-01-20 17:29:28 +0800

    optimize tools

--- agents/example-of-tools.py
+++ agents/example-of-tools.py
@@ -20,36 +20,46 @@ def chat_completions(prompt_messages, tools=None, tool_choice="auto"):
     return response.choices[0].message
 
 
-# Step 4: Define the AI prompt that needs to be passed into the completion API
-ai_prompt = [
-    {
-        "role": "assistant",
-        "content": "请逐步拆解user prompt中存在的任务,并对每一个任务调用合适的工具解决，最终给出结果",
-    },
-    {"role": "user", "content": "家里食物够吗，不够的话开车买菜天气合适吗？"},
-]
+def main():
+    # Step 4: Define the AI prompt that needs to be passed into the completion API
+    ai_prompt = [
+        {
+            "role": "assistant",
+            "content": "请逐步拆解user prompt中存在的任务,并对每一个任务调用合适的工具解决，最终给出结果",
+        },
+        {
+            "role": "user",
+            "content": "《新青年》这篇文章怎么样",
+        },
+    ]
 
-# Step 5: Extract functions dynamically based on the function_schemas
-ai_functions = [function_schemas[key] for key in available_functions.keys()]
-# Example of calling the chat function with the prompt and available functions
-first_response = chat_completions(ai_prompt, tools=ai_functions)
-print(first_response)
-tool_calls = first_response.tool_calls
-# 第一轮chat completions的结果加入prompt，再把function参数加入prompt，然后一起喂给chatgpt
-if tool_calls:
-    ai_prompt.append(first_response)
-    # 将所有函数调用的结果拼接到消息列表里
-    for tool_call in tool_calls:
-        function_name = tool_call.function.name
-        function_to_call = available_functions[function_name]
-        function_args = json.loads(tool_call.function.arguments)
-        function_response = function_to_call(**function_args)
-        ai_prompt.append(
-            {
-                "tool_call_id": tool_call.id,
-                "role": "tool",
-                "name": function_name,
-                "content": function_response,
-            }
-        )
-    print(chat_completions(ai_prompt).content)
+    # Step 5: Extract functions dynamically based on the function_schemas
+    ai_functions = [function_schemas[key] for key in available_functions.keys()]
+    # Example of calling the chat function with the prompt and available functions
+    first_response = chat_completions(ai_prompt, tools=ai_functions)
+    print(first_response)
+    tool_calls = first_response.tool_calls
+    # 第一轮chat completions的结果加入prompt，再把function参数加入prompt，然后一起喂给chatgpt
+    if tool_calls:
+        ai_prompt.append(first_response)
+        # 将所有函数调用的结果拼接到消息列表里
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_to_call = available_functions[function_name]
+            function_args = json.loads(tool_call.function.arguments)
+            function_response = function_to_call(**function_args)
+            ai_prompt.append(
+                {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": function_name,
+                    "content": function_response,
+                }
+            )
+            print(f"{function_name}:\n function_response")
+        print("汇总全部tools的输出为：")
+        print(chat_completions(ai_prompt).content)
+
+
+if __name__ == "__main__":
+    main()
--- agents/tools.py
+++ agents/tools.py
@@ -10,14 +10,18 @@ client_deepseek = OpenAI(
 
 
 # Step 3: Define a function to call ChatGPT for completions with tools
-def chat_completions(prompt_messages, tools=None, tool_choice="auto"):
+def chat_completions(prompt_messages, tools=None):
     response = client_deepseek.chat.completions.create(
         model="deepseek-chat",
         messages=prompt_messages,
         tools=tools,
-        tool_choice=tool_choice,
     )
     return response.choices[0].message
+
+
+class get_weather_scheme(BaseModel):
+    location: str = (Field(description="地址信息，如南京"),)
+    date: str = Field(description="日期信息，如20240101")
 
 
 # Step 1: Define the available functions in a dictionary
@@ -27,15 +31,17 @@ def get_weather(location, date):
             "role": "system",
             "content": "你是一个获取天气的助手，请随机杜撰一个天气查询结果",
         },
-        {"role": "user", "content": f"请查询{location}在{date}时候的天气变化"},
+        {
+            "role": "user",
+            "content": f"请查询{location}在{date}时候的天气变化",
+        },
     ]
     response = chat_completions(prompt)
     return response.content
 
 
-class get_weather_scheme(BaseModel):
-    location: str = (Field(description="地址信息，如南京"),)
-    date: str = Field(description="日期信息，如20240101")
+class get_car_power_scheme(BaseModel):
+    car: str = Field(description="具体的一辆车")
 
 
 def get_car_power(car):
@@ -50,8 +56,8 @@ def get_car_power(car):
     return response.content
 
 
-class get_car_power_scheme(BaseModel):
-    car: str = Field(description="具体的一辆车")
+class get_food_scheme(BaseModel):
+    food: str = Field(description="一种食物")
 
 
 def get_food(food):
@@ -66,8 +72,27 @@ def get_food(food):
     return response.content
 
 
-class get_food_scheme(BaseModel):
-    food: str = Field(description="一种食物")
+class get_article_outline_scheme(BaseModel):
+    article: str = Field(description="文章的主题、或者文正主体内容")
+
+
+def get_article_outline(article):
+    prompt = [
+        {
+            "role": "system",
+            "content": """
+                你是一位文本大纲生成专家，擅长根据用户的需求创建一个有条理且易于扩展成完整文章的大纲，你拥有强大的主题分析能力，能准确提取关键信息和核心要点。具备丰富的文案写作知识储备，熟悉各种文体和题材的文案大纲构建方法。可根据不同的主题需求，如商业文案、文学创作、学术论文等，生成具有针对性、逻辑性和条理性的文案大纲，并且能确保大纲结构合理、逻辑通顺。该大纲应该包含以下部分：
+                引言：介绍主题背景，阐述撰写目的，并吸引读者兴趣。
+                主体部分：第一段落：详细说明第一个关键点或论据，支持观点并引用相关数据或案例。
+                第二段落：深入探讨第二个重点，继续论证或展开叙述，保持内容的连贯性和深度。
+                第三段落：如果有必要，进一步讨论其他重要方面，或者提供不同的视角和证据。
+                结论：总结所有要点，重申主要观点，并给出有力的结尾陈述，可以是呼吁行动、提出展望或其他形式的收尾。
+                创意性标题：为文章构思一个引人注目的标题，确保它既反映了文章的核心内容又能激发读者的好奇心。""",
+        },
+        {"role": "user", "content": f"请生成{article}这篇文章的大纲"},
+    ]
+    response = chat_completions(prompt)
+    return response.content
 
 
 # Function map to easily look up function by name
@@ -75,66 +100,24 @@ available_functions = {
     "get_weather": get_weather,
     "get_car_power": get_car_power,
     "get_food": get_food,
+    "get_article_outline": get_article_outline,
 }
 
 # Step 2: Define the function schema (parameter definitions) in a separate structure
 function_schemas = {
-    "get_weather": pydantic_function_tool(get_weather_scheme),
-    "get_car_power": pydantic_function_tool(get_car_power_scheme),
-    "get_food": pydantic_function_tool(get_food_scheme),
+    "get_weather": pydantic_function_tool(model=get_weather_scheme, name="get_weather"),
+    "get_car_power": pydantic_function_tool(
+        model=get_car_power_scheme, name="get_car_power"
+    ),
+    "get_food": pydantic_function_tool(model=get_food_scheme, name="get_food"),
+    "get_article_outline": pydantic_function_tool(
+        model=get_article_outline_scheme, name="get_article_outline"
+    ),
 }
-# "get_weather": {
-#     "type": "function",
-#     "function": {
-#         "name": "get_weather",
-#         "description": "查询天气",
-#         "parameters": {
-#             "type": "object",
-#             "properties": {
-#                 "location": {
-#                     "type": "string",
-#                 },
-#                 "date": {
-#                     "type": "string",
-#                 },
-#             },
-#             "required": ["location", "date"],
-#         },
-#     },
-# },
-# "get_car_power": {
-#     "type": "function",
-#     "function": {
-#         "name": "get_car_power",
-#         "description": "查询车辆电量",
-#         "parameters": {
-#             "type": "object",
-#             "properties": {
-#                 "car": {
-#                     "type": "string",
-#                 }
-#             },
-#             "required": ["car"],
-#         },
-#     },
-# },
-# "get_food": {
-#     "type": "function",
-#     "function": {
-#         "name": "get_food",
-#         "description": "查询食物库存",
-#         "parameters": {
-#             "type": "object",
-#             "properties": {
-#                 "food": {
-#                     "type": "string",
-#                 }
-#             },
-#             "required": ["food"],
-#         },
-#     },
-# },
-# }
 
 if __name__ == "__main__":
-    print(pydantic_function_tool(get_weather_scheme))
+    import pdb
+
+    pdb.set_trace()
+    # print(pydantic_function_tool(get_weather_scheme, name="demo"))
+    print(get_article_outline("大学之道，在明明德，在新明，在止于至善"))
